01b10bfc9292f883eeb4a7a19acc9e463b2d03ab
Modupe Akanni <modupe775@gmail.com>
2024-01-12 00:33:31 +0100

    modify signup

--- chat_frontend/src/pages/SignUp.tsx
+++ chat_frontend/src/pages/SignUp.tsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <>
+        <div className="flex items-center justify-center">
+            <div className="">
+                <h1 className=''>Sign Up</h1>
+                <h3>Create an account</h3>
+                <form action="">
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input type="email" name="username" id="username" />
+                    </div>
+                    
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" />
+                    </div>
+
+                    <button type='submit'>Log In</button>
+                </form>
+            </div>
+        </div>
+    </>
+    
   )
 }
 
