7c0e00d0b6eda5846aab594986e99e94fa1f08ab
Adebesin Cell <adebesintolulope80@gmail.com>
2024-01-12 15:56:04 +0100

    adds tailwind config

--- chat_frontend/tailwind.config.js
+++ chat_frontend/tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {
-      colors: {
-        'primary': '#1DA1F2',
-        'secondary': {
-          '100': '#E1F5FE',
-          '200': '#B3E5FC',
-        }
-      }
-    },
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#1DA1F2",
+				secondary: {
+					100: "#E1F5FE",
+					200: "#B3E5FC",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
